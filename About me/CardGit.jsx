966a2cff60c11c9aaa088527b3d06e7c65e5bfff
Enzo Lacuesta <95934291+EnzoLacuesta@users.noreply.github.com>
2023-06-04 23:49:19 -0300

    Cambios en las Cards
    Hice algunos cambios de componentización y de contenido

---
+++ About me/CardGit.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const CardGit = () => {
+  return (
+    <div className="content-about-one floating-2">
+      <div className='card-about-one'>
+          <h1>Github</h1>
+          <div><ion-icon name="logo-github"  className="git"></ion-icon></div>
+          <p className='div-lorem'>
+              Uso Github para poder alojar mis proyectos y trabajar con desarrolladores de la comunidad
+          </p>
+      </div>
+    </div>
+
+
+  )
+}
+
+export default CardGit
